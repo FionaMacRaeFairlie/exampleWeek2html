d6ae4cfb39364121bf0bb892891e6a2454239055
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2026-09-22 14:51:28 +0100

    removed old files from js folder

--- js/example03.js
+++
@@ -1,9 +0,0 @@
-var currentNode;
-
-function addNewParagraph()
-{
-         currentNode = document.getElementById( "comment" );
-         let newNode = document.createElement("p");
-         newNode.appendChild(document.createTextNode("This is a new paragraph"));
-         currentNode.parentNode.insertBefore(newNode, currentNode);
- }
